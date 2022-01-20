d739751df1cca956c44fa2f9e1953acc96d1fc88
Santoshdandin <santusb427@gmail.com>
2022-01-20 22:57:24 +0530

    inputoutput processing done

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
+var outputDiv = document.querySelector("#output")
+
+
+
 
 btnTranslate.addEventListener("click", function clickEventHandler() {
-    console.log("clicked!")
-    console.log("input", txtInput.value)
+    outputDiv.innerText = "ajajsjajasjdjd" + txtInput.value
 })
